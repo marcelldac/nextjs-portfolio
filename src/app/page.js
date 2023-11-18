3c4270cd6adfe1ff849cee0eda9e9e218b00d79e
marcelldac <dacmarcell@gmail.com>
2023-11-18 14:40:12 -0300

    refactor: removes navbar

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
+    <main>
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
