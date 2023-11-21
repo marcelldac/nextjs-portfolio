07d88a208003b12f303d666f4ba00a337d7b5ee2
marcelldac <dacmarcell@gmail.com>
2023-11-21 01:32:35 -0300

    feat: modify project card

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,36 +1,29 @@
 import React from "react";
-import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import { EyeIcon } from "@heroicons/react/24/outline";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 import Link from "next/link";
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
+import { motion } from "framer-motion";
+
+const ProjectCard = ({ imgUrl, title, description, url }) => {
   return (
-    <div>
+    <motion.div whileHover={{ scale: 1.1 }}>
       <div
-        className="h-52 md:h-72 rounded-t-xl relative group"
+        className="h-52 md:h-72 rounded-t-xl items-center"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      >
-        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-          <Link
-            href={gitUrl}
-            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-            target="_blank"
-          >
-            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </Link>
-          <Link
-            href={previewUrl}
-            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-            target="_blank"
-          >
-            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+      ></div>
+
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+        <div className="flex flex-row justify-around">
+          <h5 className="text-xl font-semibold mb-2">{title}</h5>
+          <Link href={url} target="_blank">
+            <FaExternalLinkAlt width={30} height={30} />
           </Link>
         </div>
-      </div>
-      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
-        <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
