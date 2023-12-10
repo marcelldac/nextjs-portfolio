deafd05d394fb6482234eb6f2093cc09ffbc3313
marcelldac <dacmarcell@gmail.com>
2023-12-10 16:20:05 -0300

    refactor: use different name of main section

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import MainSection from "./components/MainSection";
+import Main from "./components/Main";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main>
       <div className="container mt-24 mx-auto px-12 py-4">
-        <MainSection />
+        <Main />
         <AboutSection />
         <ProjectsSection />
         <EmailSection />
