f2df8a6893a48585340c992c0dc19f95bfb2443f
marcelldac <dacmarcell@gmail.com>
2023-12-10 16:16:04 -0300

    refactor: change main component to mantine

--- src/app/components/MainSection.jsx
+++ src/app/components/MainSection.jsx
@@ -1,66 +1,64 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion, useScroll } from "framer-motion";
-import Link from "next/link";
+import { Box, Button, Grid, Image, Text } from "@mantine/core";
 
 const MainSection = () => {
   const { scrollYProgress } = useScroll();
 
   return (
-    <section className="lg:py-16">
+    <Box py="8%">
       <motion.div
         className="fixed top-0 left-0 right-0 h-[10px] bg-green-500 origin-[0%]"
         style={{ scaleX: scrollYProgress }}
       />
-      <div className="grid grid-cols-1 sm:grid-cols-12">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-        >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <TypeAnimation
-              sequence={["Marcell", 1000, "Full-stack Dev", 1000]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-              style={{ fontSize: "90px" }}
-            />
-          </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Amante de Linux e Criptomoedas. Cientista da Computação em Formação!
-          </p>
-          <div>
-            <Link
+      <Grid grow justify="center" align="stretch">
+        <Grid.Col span={{ base: 7, md: 5, lg: 9 }}>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          >
+            <Text>
+              <TypeAnimation
+                sequence={["Marcell", 1000, "Full-stack Dev", 1000]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+                style={{ fontSize: "90px" }}
+              />
+            </Text>
+            <Text mb={6}>
+              Amante de Linux e Criptomoedas. Cientista da Computação em
+              Formação!
+            </Text>
+            <Button
+              variant="filled"
+              color="green"
+              component="a"
               href="https://www.linkedin.com/in/marcell-dactes/"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-green-500 hover:bg-green-600 text-white"
               target="_blank"
             >
               Fala comigo!
-            </Link>
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
-        >
-          <div className="rounded bg-[#181818] w-[250px] h-[350px] lg:w-[300px] lg:h-[410px] relative">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
-          </div>
-        </motion.div>
-      </div>
-    </section>
+            </Button>
+          </motion.div>
+        </Grid.Col>
+        <Grid.Col span={{ base: 3, md: 5, lg: 1 }}>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="col-span-4 place-self-center mt-4 lg:mt-0"
+          >
+            <Box w="250px" h="350px" bg="gray" pos="relative">
+              <Image src="/images/hero-image.png" alt="developer image" />
+            </Box>
+          </motion.div>
+        </Grid.Col>
+      </Grid>
+    </Box>
   );
 };
 
