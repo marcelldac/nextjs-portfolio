d15ae81c82ec2db606b9cfa3c96d96d9cde7bca3
marcelldac <dacmarcell@gmail.com>
2023-12-10 17:57:48 -0300

    chore: change button default props

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,4 @@
 "use client";
-
 import { Button, Text, createTheme } from "@mantine/core";
 
 export const theme = createTheme({
@@ -14,7 +13,8 @@ export const theme = createTheme({
     Button: Button.extend({
       defaultProps: {
         variant: "filled",
-        color: "green",
+        color: "white",
+        bg: "green",
       },
     }),
   },
