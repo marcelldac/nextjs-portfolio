f885b181a81c54681e77eba77fa0ce9d25c142da
marcelldac <dacmarcell@gmail.com>
2024-01-08 02:19:09 -0300

    style: lint

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,29 @@
-import "@mantine/core/styles.css";
-import "./globals.css";
-import { MantineProvider } from "@mantine/core";
-import { Inter } from "next/font/google";
-import { theme } from "../theme";
+import '@mantine/core/styles.css';
+import './globals.css';
+import { MantineProvider } from '@mantine/core';
+import { Inter } from 'next/font/google';
+import { theme } from '../theme';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Marcell Dactes",
-  description: "Portfolio de um desenvolvedor full-stack com foco em back-end",
+  title: 'Marcell Dactes',
+  description: 'Portfolio de um desenvolvedor full-stack com foco em back-end',
 };
 
 export default function RootLayout({ children }) {
   return (
     <>
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
+        <link
+          rel='icon'
+          href='/favicon.png'
+          sizes='any'
+        />
       </head>
-      <html lang="en">
+      <html lang='en'>
         <body className={inter.className}>
-          <MantineProvider theme={theme}>{children}</MantineProvider>
+          <MantineProvider>{children}</MantineProvider>
         </body>
       </html>
     </>
